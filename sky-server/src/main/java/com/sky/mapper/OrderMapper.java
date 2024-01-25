899e1be459694253ed791c5c613414580e7753aa
xzy <2894702202@qq.com>
2024-01-25 15:36:04 +0800

    day11--销量排名统计业务代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
@@ -49,7 +50,7 @@ public interface OrderMapper {
 
     /**
      * 根据订单号和用户id查询订单
-     * @param outTradeNo
+     * @param orderNumber
      * @param userId
      * @return
      */
@@ -86,4 +87,12 @@ public interface OrderMapper {
      * @return
      */
     Integer countByMap(Map map);
+
+    /**
+     * 查询商品销量排名
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime beginTime, LocalDateTime endTime);
 }
